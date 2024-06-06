4792f1285c39eab50cdaac5f2c7669f971be8da7
Artem Batygin <aa.batygin@gmail.com>
2024-06-06 23:02:50 +0300

    Update parser

--- parser.py
+++ parser.py
@@ -1,15 +1,23 @@
-import requests
-import bs4
 import datetime
-from selenium import webdriver
+import concurrent.futures
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from bs4 import BeautifulSoup as bs
+from selenium.webdriver import Chrome
+from selenium.common.exceptions import TimeoutException
+import pandas
 
 
 def get_weather_data(start, end):
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument("--headless")
+    service = ChromeService(ChromeDriverManager().install())
+    driver = Chrome(service=service, options=options)
+
     driver.get(
         "https://www.worldweatheronline.com/saint-petersburg-weather-history/saint-petersburg-city/ru.aspx"
     )
@@ -30,27 +38,32 @@ def get_weather_data(start, end):
             By.ID, "ctl00_MainContentHolder_butShowPastWeather"
         )
         submit_button.click()
-        wait.until(
-            EC.presence_of_element_located((By.CLASS_NAME, "days-details-table"))
-        )
+        try:
+            wait.until(
+                EC.presence_of_element_located((By.CLASS_NAME, "days-details-table"))
+            )
+        except TimeoutException:
+            # If the element is not found, move to the next date
+            date += datetime.timedelta(days=1)
+            continue
 
         page_source = driver.page_source
-        soup = bs4.BeautifulSoup(page_source, "html.parser")
+        soup = bs(page_source, "html.parser")
 
         table = soup.find("table", class_="days-details-table")
-        day = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        day = [date.strftime("%Y-%m-%d"), 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         for row in table.find_all("tr")[2:]:
             cells = row.find_all(["th", "td"])
             row_data = [cell.get_text(strip=True) for cell in cells]
 
-            day[0] += float(row_data[2][:-3]) / 8  # temperature
-            day[1] += float(row_data[3][:-2]) / 8  # prepcitipation
-            day[2] += (
+            day[1] += float(row_data[2][:-3]) / 8  # temperature
+            day[2] += float(row_data[3][:-2]) / 8  # prepcitipation
+            day[3] += (
                 float(row.find_all("svg")[0]["style"][18:][:-5]) / 8
             )  # wind direction
-            day[3] += float(row_data[7][:-5]) / 8  # wind speed
-            day[4] = max(day[4], float(row_data[8][:-5]))  # gust
-            day[5] += float(row_data[6][:-2]) / 8  # preasure
+            day[4] += float(row_data[7][:-5]) / 8  # wind speed
+            day[5] = max(day[5], float(row_data[8][:-5]))  # gust
+            day[6] += float(row_data[6][:-2]) / 8  # preasure
 
         data.append(day)
         date += datetime.timedelta(days=1)
@@ -59,8 +72,28 @@ def get_weather_data(start, end):
     return data
 
 
-start = datetime.datetime(2023, 5, 4)
-end = start + datetime.timedelta(days=3)
-weather_data = get_weather_data(start, end)
-for day in weather_data:
-    print(day)
+def main():
+    start_dates = [datetime.datetime(year, 1, 1) for year in range(2019, 2024)]
+    end_dates = [datetime.datetime(year + 1, 1, 1) for year in range(2019, 2024)]
+
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [
+            executor.submit(get_weather_data, start, end)
+            for start, end in zip(start_dates, end_dates)
+        ]
+        results = [
+            pandas.DataFrame(
+                future.result(),
+                columns=["time", "tavg", "prcp", "wdir", "wspd", "wpgt", "pres"],
+            )
+            for future in concurrent.futures.as_completed(futures)
+        ]
+
+    result = pandas.concat(results)
+    result.reset_index(drop=True)
+    result.set_index("time")
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
